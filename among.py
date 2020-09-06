7637e7db5ce61b0951912179e3b0eae63c92f30c
Noé <61919992+unnnnnnnn@users.noreply.github.com>
2020-09-06 09:47:30 +0200

    Add files via upload

--- among.py
+++ among.py
@@ -1,16 +1,12 @@
 #-*- coding:utf-8 -*-
 
-#================================
-
 import discord
 import asyncio
-import emoji
-import sys
 import time
 import os
-import random
 from discord.utils import get
 from discord.ext import commands
+from consts import *
 from datetime import datetime
 
 os.chdir(os.path.dirname(__file__))
@@ -20,38 +16,6 @@ client.remove_command("help")
 with open('token.txt' ,'r') as f:
     TOKEN = f.readline()
 
-d = {'j_yellow': 'Reserved Yellow Color', 
-    'i_lime': 'Reserved Lime Color', 
-    'f_blue': 'Reserved Blue Color', 
-    'e_purple': 'Reserved Purple Color', 
-    'd_pink': 'Reserved Pink Color', 
-    'c_brown': 'Reserved Brown Color', 
-    'a_white': 'Reserved White Color', 
-    'h_green': 'Reserved Green Color', 
-    'k_orange': 'Reserved Orange Color', 
-    'b_black': 'Reserved Black Color', 
-    'g_cyan': 'Reserved Cyan Color', 
-    'l_red': 'Reserved Red Color',
-    "🇦": "Asia", 
-    "🇧": "Africa",
-    "🇨": "Europe", 
-    "🇩": "North America",
-    "🇪": "South America",
-    "🇫": "Oceania"
-}
-
-de = {
-    "🇦": "Asia", 
-    "🇧": "Africa",
-    "🇨": "Europe", 
-    "🇩": "North America",
-    "🇪": "South America",
-    "🇫": "Oceania"
-}
-
-L = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫"]
-Le = ['j_yellow', 'i_lime', 'f_blue', 'e_purple', 'd_pink', 'c_brown', 'a_white', 'h_green', 'k_orange', 'b_black', 'g_cyan', 'l_red']
-Lr = ['Reserved Yellow Color', 'Reserved Lime Color',  'Reserved Blue Color', 'Reserved Purple Color', 'Reserved Pink Color', 'Reserved Brown Color', 'Reserved White Color', 'Reserved Green Color', 'Reserved Orange Color', 'Reserved Black Color', 'Reserved Cyan Color', 'Reserved Red Color']
 
 def is_admin():
     def check_admin(ctx):
@@ -64,84 +28,105 @@ async def on_ready():
 
     await client.wait_until_ready()
     await client.change_presence(activity=discord.Game(name="The bot is among us"))
+
+    online = client.get_channel(742095974033260611)
+
     print('✅ {0.user} connecté à {1}'.format(client, datetime.now().strftime("%H:%M:%S")))
+    await online.send('✅ **{0.user}** connecté à ``{1}``'.format(client, datetime.now().strftime("%H:%M:%S")))
 
 
 @client.event
 async def on_raw_reaction_add(event):
 
-    reaction = event.emoji
+    emoji = event.emoji.name
+    channel = client.get_channel(event.channel_id)
+    message = await channel.fetch_message(event.message_id)
+    reaction = get(message.reactions, emoji=event.emoji)
     user = client.get_user(event.user_id)
     guild = client.get_guild(event.guild_id)
     member = event.member
 
-    with open('reactmsg.txt' ,'r') as f:
-        MESSAGE_ID = int(f.readline())
+    if user.bot:
+        pass
+    else:
+        check = False
+        if (event.channel_id == 751945996828672032) and (emoji in d):
+            if emoji in Le:
+                check =  any(r.name in Lr for r in member.roles)
 
-    if event.message_id == MESSAGE_ID:
-        
-        if reaction.name in d:
-            
-            check = False
-            if reaction.name in Le:
-                for r in member.roles:
-                    if r.name in Lr:
-                        channel = client.get_channel(event.channel_id)
-                        msg = await channel.fetch_message(event.message_id)
-                        await msg.remove_reaction(reaction, user)
-                        check = True
-                        break
-
-            if check == False:
-                role = discord.utils.get(guild.roles, name=d[reaction.name])
+            if check:
+                await message.remove_reaction(reaction, user)
+            else:
+                role = discord.utils.get(guild.roles, name=d[emoji])
                 await member.add_roles(role)
+                if emoji in Le:
+                    nbr = reaction.count
+                    if nbr < 5:
+                        role = discord.utils.get(guild.roles, name=dn[nbr])
+                        await member.add_roles(role)
+
+
 
 
 @client.event
 async def on_raw_reaction_remove(event):
 
-    reaction = event.emoji
+    emoji = event.emoji.name
     user = client.get_user(event.user_id)
     guild = client.get_guild(event.guild_id)
     member = guild.get_member(event.user_id)
 
-    with open('reactmsg.txt' ,'r') as f:
-        MESSAGE_ID = int(f.readline())
+    if user.bot:
+        pass
+    else:
 
-    if event.message_id == MESSAGE_ID:
-        
-        if reaction.name in d:
-            role = discord.utils.get(guild.roles, name=d[reaction.name])
-            try:
-                await member.remove_roles(role)
-            except:
-                pass
+        if event.channel_id == 751945996828672032:
+            
+            if emoji in d:
+                for r in member.roles:
+                    if r.name in list(dn.values()) and (d[emoji] in [ro.name for ro in member.roles]):
+                        role = discord.utils.get(guild.roles, name=r.name)
+                        try:
+                            await member.remove_roles(role)
+                        except:
+                            pass
+                        finally:
+                            break
+
+                role = discord.utils.get(guild.roles, name=d[emoji])
+                try:
+                    await member.remove_roles(role)
+                except:
+                    pass
 
 
 @client.command()
 @is_admin()
 async def create(ctx):
     
-    embed = discord.Embed(
-        colour = discord.Color.from_rgb(54, 57, 63),
-    ).set_footer(
-        text = "Bot Is Among Us",
-        icon_url = "https://cdn.discordapp.com/avatars/{0.id}/{0.avatar}.png?size=1024".format(client.user)
-    ).add_field(
-        name = "{} Get your roles here".format(ctx.guild.emojis[11]),
-        value = "Choose a crewmate color to reserve for the games. \nYou will receive a colored role according to your choice \n \n \n{} \nGet a **Crewmate color** role. You can only have **one** assigned color. \n \n \n{}".format(' '.join(["{}".format(ce) for ce in ctx.guild.emojis if ce.name in Le]), '\n'.join(["{}: Get the **{}** role.".format(e, d[e]) for e in de])),
-        inline = False
-    )
+    if ctx.channel.id == 751945996828672032:
 
-    panel = await ctx.channel.send(embed=embed)
-    for emoji in ctx.guild.emojis:
-        if emoji.name in Le:
+        embed = discord.Embed(
+            colour = discord.Color.from_rgb(54, 57, 63),
+        ).set_footer(
+            text = "Bot Is Among Us",
+            icon_url = "https://cdn.discordapp.com/avatars/{0.id}/{0.avatar}.png?size=1024".format(client.user)
+        ).add_field(
+            name = "{} Get your roles here".format(ctx.guild.emojis[11]),
+            value = "Choose a crewmate color to reserve for the games. \nYou will receive a colored role according to your choice \n \n \n{} \nGet a **Crewmate color** role. You can only have **one** assigned color. \n \n \n{}".format(' '.join(["{}".format(ce) for ce in ctx.guild.emojis if ce.name in Le]), '\n'.join(["{}: Get the **{}** role.".format(e, d[e]) for e in de])),
+            inline = False
+        )
+
+        await ctx.channel.purge(limit=10)
+        panel = await ctx.channel.send(embed=embed)
+        for emoji in ctx.guild.emojis:
+            if emoji.name in Le:
+                await panel.add_reaction(emoji)
+        for emoji in L:
             await panel.add_reaction(emoji)
-    for emoji in L:
-        await panel.add_reaction(emoji)
 
-    with open('reactmsg.txt' ,'w') as f:
-        f.write(str(panel.id))
+    else:
+        await ctx.channel.send("❌ Wrong channel. You have to use this command in <#{}>".format(751945996828672032))
 
 
 client.run(TOKEN)
---
+++ consts.py
@@ -0,0 +1,40 @@
+d = {'j_yellow': 'Reserved Yellow Color', 
+    'i_lime': 'Reserved Lime Color', 
+    'f_blue': 'Reserved Blue Color', 
+    'e_purple': 'Reserved Purple Color', 
+    'd_pink': 'Reserved Pink Color', 
+    'c_brown': 'Reserved Brown Color', 
+    'a_white': 'Reserved White Color', 
+    'h_green': 'Reserved Green Color', 
+    'k_orange': 'Reserved Orange Color', 
+    'b_black': 'Reserved Black Color', 
+    'g_cyan': 'Reserved Cyan Color', 
+    'l_red': 'Reserved Red Color',
+    "🇦": "Asia", 
+    "🇧": "Africa",
+    "🇨": "Europe", 
+    "🇩": "North America",
+    "🇪": "South America",
+    "🇫": "Oceania"
+}
+
+de = {
+    "🇦": "Asia", 
+    "🇧": "Africa",
+    "🇨": "Europe", 
+    "🇩": "North America",
+    "🇪": "South America",
+    "🇫": "Oceania"
+}
+
+dn = {
+    2: 'First',
+    3: 'Second',
+    4: 'Third',
+    5: 'Fourth'
+}
+
+L = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫"]
+Le = ['j_yellow', 'i_lime', 'f_blue', 'e_purple', 'd_pink', 'c_brown', 'a_white', 'h_green', 'k_orange', 'b_black', 'g_cyan', 'l_red']
+Lr = ['Reserved Yellow Color', 'Reserved Lime Color',  'Reserved Blue Color', 'Reserved Purple Color', 'Reserved Pink Color', 'Reserved Brown Color', 'Reserved White Color', 'Reserved Green Color', 'Reserved Orange Color', 'Reserved Black Color', 'Reserved Cyan Color', 'Reserved Red Color']
+
